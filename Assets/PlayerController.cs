cb9b328b660a2a4b52c436b2019d5267f3fcafc8
Kenneth Abadi <kenneth.putra25@gmail.com>
2025-02-27 19:15:28 -0800

    Added Movement and Camera Control

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,15 +2,50 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public Transform cameraTransform;  // Assign Main Camera’s transform
+    private Rigidbody rb;
+    private bool isGrounded;
+     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+    void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 direction = forward * vertical + right * horizontal;
+        rb.MovePosition(rb.position + direction * moveSpeed * Time.deltaTime);
+    }
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
